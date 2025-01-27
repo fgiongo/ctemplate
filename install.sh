e191f9e1a7543213d5d3ff7918aff1d02c30ee9c
fgiongo <fgiongo@protonmail.com>
2025-01-27 18:36:55 -0300

    More installation options
    Expanded install script
    Removed obsolete uninstall script commands

--- install.sh
+++ install.sh
@@ -1,9 +1,53 @@
 #!/bin/bash
 
-cp ctemplate /usr/local/bin
-chmod 555 /usr/local/bin/ctemplate
-mkdir -p $HOME/Templates/ctemplate
-cp Makefile-elf $HOME/Templates/ctemplate/Makefile-elf 
-cp Makefile-a $HOME/Templates/ctemplate/Makefile-a
-cp Makefile-so $HOME/Templates/ctemplate/Makefile-so
+if [ "$(id -u)" -eq 0 ]; then
+    echo "You are running this script as root! (not recommended)"
+    read -p "Continue? (Y/N): " confirm
+    [[ $confirm == [yY] ]] || exit 1
+fi
 
+install_path=${1:-"$HOME/.local/bin"}
+config_path=${2:-"$HOME/.config/ctemplate"}
+
+if [[ ! ":$PATH:" == *":$install_path:"* ]]; then
+    echo "Warning! Install path $install_path is not in PATH!"
+    read -p "Continue? (Y/N): " confirm
+    [[ $confirm == [yY] ]] || exit 1
+fi
+
+echo -e "Install path: $install_path\nConfig path: $config_path"
+
+mkdir -p $install_path || {
+    echo "Could not create install path, exiting.";
+    exit 1;
+}
+
+mkdir -p $config_path || {
+    echo "Could not create config path, exiting.";
+    uninstall();
+    exit 1;
+}
+
+cp ctemplate $install_path || {
+    echo "Could not copy executable to install path, exiting.";
+    uninstall();
+    exit 1;
+}
+
+chmod 555 $install_path/ctemplate || {
+    echo "Could not change permissions on binary, exiting.";
+    uninstall();
+    exit 1;
+}
+cp Makefile-elf $config_path/Makefile-elf || {
+    echo "Could not copy config files, exiting.";
+    uninstall();
+    exit 1;
+}
+cp Makefile-a $config_path/Makefile-a
+cp Makefile-so $config_path/Makefile-so
+
+uninstall () {
+    rm -rf $config_path;
+    rm -f $install_path/ctemplate;
+}
--- uninstall.sh
+++ uninstall.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
 
-rm /usr/local/bin/ctemplate
-rm $HOME/Templates/Makefile-elf
-rm $HOME/Templates/Makefile-a
-rm $HOME/Templates/Makefile-so
+
