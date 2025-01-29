4d1526872c78733de3c5feeb5383c650b4bab7b4
fgiongo <fgiongo@protonmail.com>
2025-01-29 19:08:23 -0300

    Install script changes, uninstall
    + Install script now creates a modified copy of the main script so that
      it contains relevant install and config paths
    
    + Implemented 'ctemplate uninstall' command
    
    + Install script now correctly searches and replaces placeholder words
      on Makefile-elf when copying it to config folder

--- install.sh
+++ install.sh
@@ -1,18 +1,22 @@
 #!/bin/bash
 
-if [ "$(id -u)" -eq 0 ]; then
-    echo "You are running this script as root! (not recommended)"
-    read -p "Continue? (Y/N): " confirm
-    [[ $confirm == [yY] ]] || exit 1
-fi
+
+uninstall () {
+    rm -rf $config_path
+    rm -f $install_path/ctemplate
+}
+
 
 install_path=${1:-"$HOME/.local/bin"}
 config_path=${2:-"$HOME/.config/ctemplate"}
 
+if [ "$(id -u)" -eq 0 ]; then
+    echo "Do not run this script as root! Exiting"
+    exit 1
+fi
+
 if [[ ! ":$PATH:" == *":$install_path:"* ]]; then
-    echo "Warning! Install path $install_path is not in PATH!"
-    read -p "Continue? (Y/N): " confirm
-    [[ $confirm == [yY] ]] || exit 1
+    echo -e "Warning! Install path $install_path is not in PATH!\nPlease include $install_path in PATH."
 fi
 
 echo -e "Install path: $install_path\nConfig path: $config_path"
@@ -23,31 +27,37 @@ mkdir -p $install_path || {
 }
 
 mkdir -p $config_path || {
-    echo "Could not create config path, exiting.";
-    uninstall();
+    echo "Could not create config path, uninstalling.";
+    uninstall;
     exit 1;
 }
 
-cp ctemplate $install_path || {
-    echo "Could not copy executable to install path, exiting.";
-    uninstall();
+cp ctemplate ctemplate.copy
+sed -i "1a\\\ninstall_path=\"$install_path\"\nconfig_path=\"$config_path\"" ctemplate.copy
+
+cp ctemplate.copy $install_path/ctemplate || {
+    echo "Could not copy executable to install path, uninstalling.";
+    uninstall;
     exit 1;
 }
+
+rm -f ctemplate.copy
 
 chmod 555 $install_path/ctemplate || {
-    echo "Could not change permissions on binary, exiting.";
-    uninstall();
+    echo "Could not change permissions on binary, uninstalling.";
+    uninstall;
     exit 1;
 }
-cp Makefile-elf $config_path/Makefile-elf || {
-    echo "Could not copy config files, exiting.";
-    uninstall();
+
+touch $config_path/ctpconf || {
+    echo "Could not create config files, uninstalling.";
+    uninstall;
     exit 1;
 }
+
+echo -e "install_path=\"$install_path\"\nconfig_path=\"$config_path\"" > $config_path/ctpconf
+
+cp Makefile-elf $config_path/Makefile-elf 
 cp Makefile-a $config_path/Makefile-a
 cp Makefile-so $config_path/Makefile-so
 
-uninstall () {
-    rm -rf $config_path;
-    rm -f $install_path/ctemplate;
-}
