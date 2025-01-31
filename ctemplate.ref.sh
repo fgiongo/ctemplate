55f699239539f2ef64e8e29af0042126327cd524
fgiongo <fgiongo@protonmail.com>
2025-01-31 14:43:26 -0300

    Feats: ctemplate help, ctemplate version

--- ctemplate.ref.sh
+++ ctemplate.ref.sh
@@ -1,13 +1,62 @@
 #!/bin/bash
 
-show_help () {
+ctemplate_version="1.0.0"
+
+ctemplate_show_help () {
     cat << EOF
-Usage: $1 <command> [OPTIONS] 
+Usage: $1 [-h] [--help] [-v] [--version] <command> [options] 
 
 Commands: 
   help                  Show this help message
   version               Show version number
   new <name>            Initializes a new C project on working directory
   module                C module handling subcommand
+
+Options:
+  --no-git              Don't initialize new git repository
 EOF
+exit 0
 }
+
+
+ctemplate_show_version () {
+    echo "ctemplate version $ctemplate_version"
+    exit 0
+}
+
+
+ctemplate_new () {
+    exit 0
+}
+
+
+ctemplate_module () {
+    exit 0
+}
+
+case $1 in
+
+    "help" | "-h" | "--help")
+        ctemplate_show_help
+        ;;
+
+    "version" | "-v" | "--version")
+        ctemplate_show_version
+        ;;
+
+    "new")
+        shift
+        ctemplate_new "$@"
+        ;;
+
+    "module")
+        shift
+        ctemplate_module "$@"
+        ;;
+
+    *)
+        echo "Usage: $1 [-h] [--help] [-v] [--version] <command> [options]"
+        exit 1
+        ;;
+
+esac
